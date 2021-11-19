6b37755d2f1eeb76b02101984a7dbcbc6a339a95
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-11-19 16:50:24 -0300

    feat: add style page cart

--- src/pages/cart/CartProducts.js
+++ src/pages/cart/CartProducts.js
@@ -1,23 +1,27 @@
-import React from 'react';
-
 import CartItem from './CartItem';
 import { useCart } from '../../hooks/useCart';
 
 const CartProducts = () => {
 
-  const { cartItems } = useCart();
+  const { cartItems, clearCart } = useCart();
 
   return (
-    <div>
-      <div>
-
-        {
-          cartItems.map(product => <CartItem key={ product.id } product={ product } />)
-        }
-
+    <>
+      {
+        cartItems.map(product => <CartItem key={ product.id } product={ product } />)
+      }
+      <div className="flex justify-end">
+        <button
+          type="submit"
+          onClick={ clearCart }
+          className="w-22 h-9 mb-6 bg-gray-400 border border-transparent rounded-md shadow-sm 
+                        py-2 px-4 text-sm font-medium text-white hover:bg-gray-600 focus:outline-none 
+                          focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          Limpar carrinho
+        </button>
       </div>
-    </div>
-
+    </>
   );
 };
 
